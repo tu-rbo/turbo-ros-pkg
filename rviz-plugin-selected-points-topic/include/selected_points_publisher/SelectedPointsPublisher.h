307efcfae87d44aeefc73982a76186469328898d
roberto-martinmartin <rmartinmar@gmail.com>
2015-02-17 13:47:09 +0100

    The plugin now subscribes to a point cloud topic to get the real point cloud. It uses rviz to build an oriented bounding box around the rviz selected points. Then it filters the complete point cloud through this bounding box to get the real set of points. The problem was that rviz only selects the points that project into the image plane, and therefore, many points are not selected because they are occluded by other points. The only problem in the plugin right now is that if rviz does not select points in the real boundaries of the bounding box, the box will be a bit smaller that it should be (only containing the points that rviz could select). The perspective when selecting points should be carefully set up

--- rviz-plugin-selected-points-topic/include/selected_points_publisher/SelectedPointsPublisher.h
+++ rviz-plugin-selected-points-topic/include/selected_points_publisher/SelectedPointsPublisher.h
@@ -45,8 +45,8 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef SELECTED_POINTS_TOPIC_H
-#define SELECTED_POINTS_TOPIC_H
+#ifndef SELECTED_POINTS_PUBLISHER_H
+#define SELECTED_POINTS_PUBLISHER_H
 
 #ifndef Q_MOC_RUN  // See: https://bugreports.qt-project.org/browse/QTBUG-22829
 # include <ros/node_handle.h>
@@ -60,17 +60,21 @@
 
 #include "rviz/default_plugin/tools/selection_tool.h"
 
-namespace rviz_plugin_selected_points_topic
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <sensor_msgs/PointCloud2.h>
+
+namespace rviz_plugin_selected_points_publisher
 {
 
-class SelectedPointsTopic;
+class SelectedPointsPublisher;
 
-class SelectedPointsTopic : public rviz::SelectionTool
+class SelectedPointsPublisher : public rviz::SelectionTool
 {
 Q_OBJECT
 public:
-  SelectedPointsTopic();
-  virtual ~SelectedPointsTopic();
+  SelectedPointsPublisher();
+  virtual ~SelectedPointsPublisher();
 
   /*
    * Hooks on rviz::SelectionTool::processMouseEvent() to get and publish
@@ -78,22 +82,37 @@ public:
    */
   virtual int processMouseEvent( rviz::ViewportMouseEvent& event );
 
+  virtual int processKeyEvent( QKeyEvent* event, rviz::RenderPanel* panel );
+
 public Q_SLOTS:
   /*
    * Creates the ROS topic
    */
   void updateTopic();
 
-protected:
-  ros::NodeHandle nh_;
-  ros::Publisher pub_;
-  std::string tf_frame_;
-  std::string cloud_topic_;
-  bool selecting_;
-};
-} // end namespace rviz_plugin_selected_points_topic
+  void PointCloudsCallback(const sensor_msgs::PointCloud2ConstPtr &pc_msg);
 
-#endif // SELECTED_POINTS_TOPIC_H
+protected:
+
+  int _processSelectedAreaAndPublishPoints();
+  ros::NodeHandle nh_;
+  ros::Publisher rviz_selected_pub_;
+  ros::Publisher real_selected_pub_;
+  ros::Publisher bb_marker_pub_;
+  ros::Subscriber pc_subs_;
+
+  std::string tf_frame_;
+  std::string rviz_cloud_topic_;
+  std::string real_cloud_topic_;
+  std::string subs_cloud_topic_;
+  std::string bb_marker_topic_;
+  bool selecting_;
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr current_pc_;
+};
+} // end namespace rviz_plugin_selected_points_publisher
+
+#endif // SELECTED_POINTS_PUBLISHER_H
 
 ////////////////////////////////////////////////////////////////////////////////
 
